2e8b56fbd34e9f7831cc5791a7c69e17df98f689
rsgilbert <gssenyonjo@gmail.com>
2025-05-06 07:00:49 +0300

    solve bitplusplus

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿BitPlusPlus.SolveProblem();
+
+public static class BitPlusPlus
+{
+    public static void SolveProblem()
+    {
+        IEnumerable<string> lines = GetLines();
+        int finalValueOfX = lines.Sum(ValueOfX);
+        Console.WriteLine(finalValueOfX);
+    }
+
+    private static int ValueOfX(string statement)
+    {
+        switch (statement)
+        {
+            case "X++":
+            case "++X":
+                return 1;
+            case "X--":
+            case "--X":
+                return -1;
+            default:
+                throw new Exception("Illegal state");
+        }
+    }
+
+
+
+    private static IEnumerable<string> GetLines()
+    {
+
+        string? line = Console.In.ReadLine();
+        ArgumentNullException.ThrowIfNull(line);
+        int lineCount = int.Parse(line);
+
+        for (int i = 0; i < lineCount; i++)
+        {
+            line = Console.In.ReadLine();
+            ArgumentNullException.ThrowIfNull(line);
+            yield return line;
+        }
+    }
+
+
+}
